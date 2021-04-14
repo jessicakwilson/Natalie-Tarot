a97bfa09dcd2ac9ec4af6e90db3aa1f216c03cf5
Jessica Wilson <44528514+jessicakwilson@users.noreply.github.com>
2021-04-14 15:50:27 -0400

    use images for cards and finish basic functionality

--- main.js
+++ main.js
@@ -2,7 +2,38 @@
 
 /*global console*/
 /*eslint no-console: "off"*/
-console.log("hello world");
+
+/* Random integer in range [0, max) */
+function randint(max) {
+    return Math.floor(Math.random() * max);
+}
+
+var imageNames = [
+    "0-the-fool-2.0.jpg",
+    "1-the-magician-new.jpg",
+    "2-high-priestess-2.0.jpg",
+    "3-the-empress-final.jpg",
+    "4-emperor-4.0.jpg",
+    "5-hierophant-final3.jpg",
+    "6-the-lovers-1.0.jpg",
+    "7-the-chariot-3.2.jpg",
+    "8-strength.jpg",
+    "9-the-hermit-final.jpg",
+    "10-wheel-of-fortune-FINAL.jpg",
+    "11-justice-2.1.jpg",
+    "12-hanged-man-2.2.jpg",
+    "13-death-3.1.jpg",
+    "14-temperance-3.0-final.jpg",
+    "15-the-devil.jpg",
+    "16-the-tower-final.jpg",
+    "17-the-star-3.5.jpg",
+    "18-the-moon.jpg",
+    "19-the-sun-2.2-final.jpg",
+    "20-judgement-2.1.jpg",
+    "21-the-world-final.jpg"
+];
+
+var alreadySelectedCards = [];
 
 var cards = [
     document.getElementById("card1"),
@@ -31,23 +62,37 @@ var cards = [
 
 function OnCardClick(e) {
     var card = e.target;
-    card.classList.toggle("selected");
-    console.log("selected ", card.id);
+    var cardNum = cards.indexOf(card);
+    
+    var overlay = document.getElementById("overlay");
+    overlay.style.display = "block";
+    
+    var cardFront = document.getElementById("CardFront");
+    
+    // card was already selected
+    if (alreadySelectedCards[cardNum] !== undefined) {  
+        cardFront.style.backgroundImage = "url(" + "'images/" + alreadySelectedCards[cardNum] + "')";
+        card.style.backgroundImage = "url(" + "'images/" + alreadySelectedCards[cardNum] + "')";
+        return;
+    }
+    
+    // randomly choose a card
+    var chosenCard = randint(imageNames.length);  
+    cardFront.style.backgroundImage = "url(" + "'images/" + imageNames[chosenCard] + "')";
+    card.style.backgroundImage = "url(" + "'images/" + imageNames[chosenCard] + "')";
+    
+    alreadySelectedCards[cardNum] = imageNames[chosenCard];  // keep track of chosen cards
+    imageNames.splice(chosenCard, 1);  // remove card from options
 }
 
 cards.forEach(function (c) {
     c.onclick = OnCardClick;
 });
 
-
-var nextButton = document.getElementById("NextButton");
-nextButton.onclick = function() {
-    var overlay = document.getElementById("overlay");
-    overlay.style.display = "block";
-}
-
-var backButton = document.getElementById("BackButton");
-backButton.onclick = function() {
+function hideOverlay() {
     var overlay = document.getElementById("overlay");
     overlay.style.display = "none";
 }
+
+var backButton = document.getElementById("BackButton");
+backButton.onclick = hideOverlay;
